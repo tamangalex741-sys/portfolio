db6787508c802e8dca2c29f5041de99770e5e632
tamangalex741-sys <tamangalex741@gmail.com>
2026-04-30 08:36:01 +0545

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+function changeText() {
+           let a= document.querySelector(".p-two");
+		  a.style.color="green";
+        }
+		function conformMsg(){
+		confirm("Your name is correct");
+		}
