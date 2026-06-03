dd2b8c8253376f9620bb37bc42502766ff63016e
Alex Tamang <tamangalex741@gmail.com>
2026-06-03 20:43:10 +0545

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,19 @@
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
+
+// Close menu when link clicked
+document.querySelectorAll(".nav-link a").forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navLinks.classList.remove("active");
+    });
+});
 function changeText() {
            let a= document.querySelector(".p-two");
 		  a.style.color="green";
